5081c50a9af4110a78ca06639979a4531a88cc86
dwhenson <dave.henson@gmail.com>
2022-04-25 10:17:55 +0300

    🐛 FIX: validation errors

--- lesson-1.js
+++ lesson-1.js
@@ -15,13 +15,13 @@ function createComputer() {
       let randomIndex = Math.floor(Math.random() * choices.length);
       let compMove = RPSGame.KEY_TO_CHOICE[choices[randomIndex][0]];
       if (compMove === RPSGame.human.move) {
-        if (RPSGame.noTie()) {
+        if (RPSGame.assistComputer()) {
           return this.choose();
         } else {
           return (this.move = compMove);
         }
       }
-      this.move = compMove;
+      return (this.move = compMove);
     },
   };
   return Object.assign(playerObject, computerObject);
@@ -36,11 +36,11 @@ function createHuman() {
 
       while (true) {
         console.log(
-          "Please choose rock (r), paper (p), scissors (s), lizard (l), spock (k):"
+          ">>> Please choose rock (r), paper (p), scissors (s), lizard (l), spock (k):\n"
         );
         choice = READLINE.question();
-        if (Object.keys(RPSGame.KEY_TO_CHOICE).includes(choice[0])) break;
-        console.log("Sorry, invalid choice.");
+        if (Object.keys(RPSGame.KEY_TO_CHOICE).includes(choice)) break;
+        console.log(">>> Sorry, invalid choice.\n");
       }
 
       this.move = RPSGame.KEY_TO_CHOICE[choice];
@@ -50,7 +50,12 @@ function createHuman() {
   return Object.assign(playerObject, humanObject);
 }
 
+/* ====================================================
+   Game Object
+   ==================================================== */
 const RPSGame = {
+  /* Properties
+  /* ==================================================== */
   VALID_CHOICES: ["rock (r)", "paper (p)", "scissors (s)", "lizard (l)", "spock (k)"],
   KEY_TO_CHOICE: {
     r: "rock",
@@ -66,6 +71,8 @@ const RPSGame = {
     lizard: ["paper", "spock"],
     spock: ["rock", "scissors"],
   },
+  ASSIST_COMPUTER: 0.6,
+  TOURNAMENT_WIN: 5,
 
   human: createHuman(),
   computer: createComputer(),
@@ -78,44 +85,12 @@ const RPSGame = {
     computerMoves: [],
   },
 
-  noTie() {
+  /* Helper functions
+  /* ==================================================== */
+  assistComputer() {
     if (this.moveHistory.humanMoves.length < 2) return false;
-    return Number(this.score.human / this.moveHistory.humanMoves.length) > 0;
-  },
-
-  displayWelcomeMessage() {
-    console.log("Welcome to Rock, Paper, Scissors, Lizard, Spock!");
-    console.log("The first player to win five rounds wins the tournament.");
-  },
-
-  playerWins(choice, computerChoice) {
-    return this.WINNING_COMBOS[choice].includes(computerChoice);
-  },
-
-  displayWinner() {
-    let humanMove = this.human.move;
-    let computerMove = this.computer.move;
-
-    console.log(`You chose: ${this.human.move}`);
-    console.log(`The computer chose: ${this.computer.move}`);
-
-    this.moveHistory.humanMoves.push(humanMove);
-    this.moveHistory.computerMoves.push(computerMove);
-
-    if (this.playerWins(humanMove, computerMove)) {
-      this.score.human += 1;
-      console.log("You win!");
-    } else if (humanMove === computerMove) {
-      console.log("It's a tie");
-    } else {
-      this.score.computer += 1;
-      console.log("Computer wins!");
-    }
-  },
-
-  displayScore() {
-    console.log(
-      `The current scores are: Human: ${this.score.human}, Computer ${this.score.computer}\n`
+    return (
+      Number(this.score.human / this.moveHistory.humanMoves.length) > this.ASSIST_COMPUTER
     );
   },
 
@@ -130,33 +105,84 @@ const RPSGame = {
     };
   },
 
+  /* Game functions
+  /* ==================================================== */
+  displayWelcomeMessage() {
+    console.log("Welcome to Rock, Paper, Scissors, Lizard, Spock!");
+    console.log(
+      `The first player to win ${this.TOURNAMENT_WIN} rounds wins the tournament.\n`
+    );
+  },
+
+  playerWins(choice, computerChoice) {
+    return this.WINNING_COMBOS[choice].includes(computerChoice);
+  },
+
+  displayWinner() {
+    let humanMove = this.human.move;
+    let computerMove = this.computer.move;
+
+    console.log(`>>> Round ${this.moveHistory.humanMoves.length + 1}`);
+    console.log("------------");
+    console.log(`You chose: ${this.human.move}`);
+    console.log(`The computer chose: ${this.computer.move}`);
+
+    this.moveHistory.humanMoves.push(humanMove);
+    this.moveHistory.computerMoves.push(computerMove);
+
+    if (this.playerWins(humanMove, computerMove)) {
+      this.score.human += 1;
+      console.log(">>> You win!\n");
+    } else if (humanMove === computerMove) {
+      console.log(">>> It's a tie\n");
+    } else {
+      this.score.computer += 1;
+      console.log(">>> Computer wins!\n");
+    }
+  },
+
+  displayScore() {
+    console.log(
+      `The current scores are: Human: ${this.score.human}, Computer ${this.score.computer}`
+    );
+    console.log("---------------------------------------------\n");
+  },
+
   checkTournamentWinner() {
-    if (this.score.computer === 5) {
-      console.log("Computer wins the tournament!!");
+    if (this.score.computer === this.TOURNAMENT_WIN) {
+      console.log(">>> Computer wins the tournament!! <<<");
+      console.log("--------------------------------------\n");
       this.reset();
       return false;
     }
-    if (this.score.human === 5) {
-      console.log("You win the tournament!!");
+    if (this.score.human === this.TOURNAMENT_WIN) {
+      console.log(">>> You win the tournament!! <<<");
+      console.log("--------------------------------\n");
       this.reset();
       return false;
     }
+    return true;
   },
 
   playAgain() {
-    console.log("Would you like to play again? (y/n)");
+    console.log(">>> Would you like to play again? (y/n)\n");
     let answer = READLINE.question();
     while (!["y", "n"].includes(answer)) {
-      console.log("Sorry, invalid choice.");
+      console.log(">>> Sorry, invalid choice.");
       answer = READLINE.question();
     }
-    return answer.toLowerCase()[0] === "y";
+    if (answer.toLowerCase()[0] === "y") {
+      console.clear();
+      return true;
+    }
   },
 
   displayGoodbyeMessage() {
-    console.log("Thanks for playing Rock, Paper, Scissors. Lizard, Spock.\nGoodbye!");
+    console.log("Thanks for playing Rock, Paper, Scissors. Lizard, Spock.\nGoodbye!\n");
   },
 
+  /* Procedure
+  /* ==================================================== */
   play() {
     this.displayWelcomeMessage();
 
@@ -174,4 +200,7 @@ const RPSGame = {
   },
 };
 
+/* ====================================================
+   Inits and Event Listeners
+   ==================================================== */
 RPSGame.play();
