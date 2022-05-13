ad8e7df5dbc3cd77b3a087051ca6fb0453794435
dwhenson <dave.henson@gmail.com>
2022-05-13 18:06:00 +0300

    ✅ ADD: exercises - easy

---
+++ Exercises/01-Easy/03.js
@@ -0,0 +1,18 @@
+class Cat {
+  constructor(name) {
+    this.name = name;
+  }
+  speaks() {
+    return `${this.name} says meowwww.`;
+  }
+}
+
+let fakeCat = Object.create(Cat.prototype);
+// let fakeCat = new Cat();
+
+console.log(fakeCat instanceof Cat);
+// logs true
+console.log(fakeCat.name);
+// logs undefined
+console.log(fakeCat.speaks());
+// logs undefined says meowwww.
